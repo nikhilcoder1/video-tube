d8109bcd7efd201ed3a72828a11b2fb53c510bb5
dev_nick <developernikhil28@gmail.com>
2025-10-31 21:40:24 +0530

    Route imported and added in app.js

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 
+// Importing routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+
+
+// Using routes
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 export { app };
