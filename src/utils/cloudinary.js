604856163a073e8cea09be88a8871be3054628f8
dev_nick <developernikhil28@gmail.com>
2025-11-03 22:01:12 +0530

    Cloudinary credentials removed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,13 +11,6 @@ console.log("Cloudinary ENV check =>", {
 });
 
 
-// configure cloudinary with your credentials
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath) return null; 
