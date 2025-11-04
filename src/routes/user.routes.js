09abf8f06bd8b41fb2aa2fcb02c84962adac3ce9
dev_nick <developernikhil28@gmail.com>
2025-11-05 03:00:03 +0530

    Secured route for logout user added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser , logoutUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -10,5 +11,8 @@ router.route("/register").post(
         { name: "coverImage", maxCount: 1 }
     ]),
     registerUser);
+
+// Protected route
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
