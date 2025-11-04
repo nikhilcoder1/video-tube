c8efae3af76e6bda427e717bbb0791650bc1bcb3
dev_nick <developernikhil28@gmail.com>
2025-11-05 02:59:25 +0530

    Logout user controller added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -7,7 +7,6 @@ import {
   deleteFromCloudinary,
 } from "../utils/cloudinary.js";
 import fs from "fs";
-
 import jwt from "jsonwebtoken";
 
 const genereateAccessAndRefreshToken = async (userId) => {
@@ -182,6 +181,36 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined},
+    },
+    {
+      new: true,
+    }
+  )    
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(
+    new ApiResponse(
+      200,
+      null,
+      "User logged out successfully"
+    )
+  );
+});
+
+
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
 
@@ -230,4 +259,5 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser , refreshAccessToken };
+
+export { registerUser, loginUser , refreshAccessToken , logoutUser };
