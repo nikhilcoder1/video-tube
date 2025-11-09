ce0c4376a2c06e555093304f32c1de06b3f3307b
dev_nick <developernikhil28@gmail.com>
2025-11-09 12:27:52 +0530

    User routes addes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
-import { registerUser , logoutUser } from "../controllers/user.controllers.js";
+import { registerUser , 
+    logoutUser, 
+    loginUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+}
+from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
+
+// unsecured routes : can be accessed by anyone i.e. no need to implement verifyJWT
 
 router.route("/register").post(
     upload.fields([
@@ -12,7 +26,17 @@ router.route("/register").post(
     ]),
     registerUser);
 
+router.route("/login").post(loginUser)
+
 // Protected route
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
