b150aa464fc559697a97f0059c01213a71486139
dev_nick <developernikhil28@gmail.com>
2025-11-10 23:21:58 +0530

    Few fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -430,7 +430,6 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     ));
 });
 
-
 const getWatchHistory = asyncHandler(async(req, res) => {
     const user = await User.aggregate([
         {
